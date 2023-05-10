6d7b6fd04146f9f488ec180290bdc472790c5aa0
parkmiri <parkmiri0722@gmail.com>
2023-05-11 01:40:51 +0900

    test

---
+++ practice/src/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println("박미리");
+        System.out.println("20070722");
+        System.out.println("parkmiri0722@gmail.com");
+    }
+}
