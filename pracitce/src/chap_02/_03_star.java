f6c969a809d141721278c9c2a9338d6bf1d7fbbc
parkmiri <parkmiri0722@gmail.com>
2023-06-03 23:53:47 +0900

    updateJavaPractice

---
+++ pracitce/src/chap_02/_03_star.java
@@ -0,0 +1,25 @@
+package chap_02;
+
+public class _03_star {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 -i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println( );
+        }
+
+    }
+}
