85aa4349d31dd98a9e0257a778577d58ccc30644
parkmiri <parkmiri0722@gmail.com>
2023-06-06 22:52:55 +0800

    update

---
+++ pracitce/src/chap03/_01_while.java
@@ -0,0 +1,13 @@
+package chap03;
+public class _01_while {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a<2) {
+            System.out.println("미리야");
+            a++;
+        }
+        while (true) {
+            System.out.println("안녕");
+        }
+    }
+    }
---
+++ pracitce/src/chap03/_02_if.java
@@ -0,0 +1,20 @@
+package chap03;
+
+public class _02_if {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(4);
+        }
+        if (false) {
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(4);
+        }
+        System.out.println(5);
+    }
+}
+
